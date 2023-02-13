deade281090cd1a9d4aef721a2a6f539d77d7497
bchillman <bhillman217@gmail.com>
2023-02-13 13:39:17 -0500

    Adding Part 1 file and images.

---
+++ MechaCarChallenge.R
@@ -0,0 +1,6 @@
+library(dplyr)
+MechaCar_mpg <- read.csv("Data/MechaCar_mpg.csv")
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
