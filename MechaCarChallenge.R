48047fecf6695787cb56efb809b6396807862f89
bchillman <bhillman217@gmail.com>
2023-02-13 14:30:47 -0500

    Adding Part 2 file and images.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,14 @@
+# Part 1
 library(dplyr)
+library(tidyverse)
 MechaCar_mpg <- read.csv("Data/MechaCar_mpg.csv")
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+# Part 2
+Suspension_Coil <- read.csv("Data/Suspension_Coil.csv")
+total_summary <- summarize(Suspension_Coil, Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
